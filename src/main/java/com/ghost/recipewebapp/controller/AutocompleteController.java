5dc25e2e55920daf4b6f6e5da521c55a1fca3a04
ghost <bodikandrusenko@gmail.com>
2022-09-11 00:51:08 +0300

    Add search by recipe.title in nav bar

--- src/main/java/com/ghost/recipewebapp/controller/AutocompleteController.java
+++ src/main/java/com/ghost/recipewebapp/controller/AutocompleteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/search")
@@ -20,8 +21,15 @@ public class AutocompleteController {
     @Autowired
     private AutocompleteService autocompleteService;
 
+    // returns "label" for Ingredient.product
     @GetMapping("/ingredients")
     public ResponseEntity<List<String>> searchProducts(@RequestParam("product") @NotBlank String input) {
         return new ResponseEntity<>(autocompleteService.getProductsContainsStr(input), HttpStatus.OK);
+    }
+
+    // returns "value" and "label" for Recipe.title
+    @GetMapping("/titles")
+    public ResponseEntity<List<Map<String, String>>> searchTitlesAndId(@RequestParam("title") @NotBlank String input) {
+        return new ResponseEntity<>(autocompleteService.getTitlesContainsStr(input), HttpStatus.OK);
     }
 }
--- src/main/java/com/ghost/recipewebapp/service/impl/AutocompleteService.java
+++ src/main/java/com/ghost/recipewebapp/service/impl/AutocompleteService.java
@@ -1,23 +1,44 @@
 package com.ghost.recipewebapp.service.impl;
 
+import com.ghost.recipewebapp.entity.Recipe;
 import com.ghost.recipewebapp.repository.IngredientCustomRepository;
 import com.ghost.recipewebapp.repository.IngredientCustomRepositoryImpl;
+import com.ghost.recipewebapp.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AutocompleteService {
     private final IngredientCustomRepository ingredientRepository;
+    private final RecipeRepository recipeRepository;
 
     @Autowired
-    public AutocompleteService(IngredientCustomRepositoryImpl ingredientRepository) {
+    public AutocompleteService(IngredientCustomRepositoryImpl ingredientRepository, RecipeRepository recipeRepository) {
         this.ingredientRepository = ingredientRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     // returns Set of Ingredient.product that contains param string
-    public List<String> getProductsContainsStr(String input){
+    public List<String> getProductsContainsStr(String input) {
         return ingredientRepository.findProductsContainsStr(input);
+    }
+
+    // returns map of recipe title and id where title contains param string
+    public List<Map<String, String>> getTitlesContainsStr(String input) {
+        List<Recipe> recipeList = recipeRepository.findFirst10ByTitleContainingIgnoreCase(input);
+
+        List<Map<String, String>> result = new ArrayList<>();
+        for (Recipe recipe: recipeList) {
+            result.add(Map.of(
+                    "label", recipe.getTitle(),
+                    "value", recipe.getId().toString()
+            ));
+        }
+
+        return result;
     }
 }
