044fd9ffcdac22a98b31af9dca672dd34bcf33d9
ghost <bodikandrusenko@gmail.com>
2022-09-11 21:01:26 +0300

    Add validation messages to autocomplete controller

--- src/main/java/com/ghost/recipewebapp/controller/AutocompleteController.java
+++ src/main/java/com/ghost/recipewebapp/controller/AutocompleteController.java
@@ -23,13 +23,17 @@ public class AutocompleteController {
 
     // returns "label" for Ingredient.product
     @GetMapping("/ingredients")
-    public ResponseEntity<List<String>> searchProducts(@RequestParam("product") @NotBlank String input) {
+    public ResponseEntity<List<String>> searchProducts(@RequestParam("product")
+                                                       @NotBlank(message = "product param is required")
+                                                               String input) {
         return new ResponseEntity<>(autocompleteService.getProductsContainsStr(input), HttpStatus.OK);
     }
 
     // returns "value" and "label" for Recipe.title
     @GetMapping("/titles")
-    public ResponseEntity<List<Map<String, String>>> searchTitlesAndId(@RequestParam("title") @NotBlank String input) {
+    public ResponseEntity<List<Map<String, String>>> searchTitlesAndId(@RequestParam("title")
+                                                                       @NotBlank(message = "title param is required")
+                                                                               String input) {
         return new ResponseEntity<>(autocompleteService.getTitlesContainsStr(input), HttpStatus.OK);
     }
 }
